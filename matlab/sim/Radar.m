14637b5768cd160d2c05a37a0c91a7f0aad06664
Steven <level2fast@gmail.com>
2024-01-07 20:51:04 -0800

    Updated class variables to use Pascal Case naming convention and add Target class
    Updated class variables to use Pascal Case naming convention and add Target class

--- matlab/sim/Radar.m
+++ matlab/sim/Radar.m
@@ -6,22 +6,22 @@ classdef Radar
         Pt_watts  (1,1) {mustBeNumeric} = 0   % Peak Transmit Power Watts
         Gt_db     (1,1) {mustBeNumeric} = 0   % Gain of transmitter
         Gr_db     (1,1) {mustBeNumeric} = 0   % Gain of receiver
-        lambda_m  (1,1) {mustBeNumeric} = 0   % wavelength
+        Lambda_m  (1,1) {mustBeNumeric} = 0   % wavelength
         R_m       (1,1) {mustBeNumeric} = 0   % range betweed radar and target
         T_celc    (1,1) {mustBeNumeric} = 290 % operating temperature
         F_db      (1,1) {mustBeNumeric} = 0   % receiver noise figure
         L_db      (1,1) {mustBeNumeric} = 0   % system path loss
 
         % motion properties
-        Plat_pos_m   {mustBeReal, mustBeVector} = [0;0;0;]
-        Plat_vel_m_s {mustBeReal, mustBeVector} = [0;0;0;]
+        Plat_Pos_m   {mustBeReal, mustBeVector} = [0;0;0;]
+        Plat_Vel_m_s {mustBeReal, mustBeVector} = [0;0;0;]
 
         % Waveform properties
         Bandwidth_hz   (1,1) {mustBeNumeric} = 0 % bandwidth
         Fs_hz          (1,1) {mustBeNumeric} = 0 % sampling rate
         Prf_hz         (1,1) {mustBeNumeric} = 0 % pulse repitition frequency
-        Pulse_width_s  (1,1) {mustBeNumeric} = 0 % transmitted pulse width/duration
-        Freq_center_hz (1,1) {mustBeNumeric} = 0 % radar carrier frequency
+        Pulse_Width_s  (1,1) {mustBeNumeric} = 0 % transmitted pulse width/duration
+        Freq_Center_hz (1,1) {mustBeNumeric} = 0 % radar carrier frequency
 
         % Dwell properties
         Tcpi_s         {mustBeReal} = 0 % radar CPI time depends on the time 
@@ -29,8 +29,8 @@ classdef Radar
         N_pulses       {mustBeReal} = 0 % number of pulses per dwell 
                                     
         % Parameter selection
-        Duty_factor        {mustBeReal} = 0 % ratio of pulse width to the period
-        Antenna_diameter_m {mustBeReal} = 0 % length of antenna
+        Duty_Factor        {mustBeReal} = 0 % ratio of pulse width to the period
+        Antenna_Diameter_m {mustBeReal} = 0 % length of antenna
     end
     properties(Access=protected)
         Boltzman {mustBeNumeric} = physconst('Boltzmann')  % boltzman constant
---
+++ matlab/sim/radar_simulation.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Radar Target Generation and Detection Simulator
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Initialize radar and target parameters
+
+% Generate FMCW signal
+
+% Apply migration factor to target to simulate movement(i.e. change in
+% doppler frequency over time)
+
+% Perform range and doppler detection and produce range doppler map
+
+% Common False Alarm Rate(CFAR) detection
+% The role of the constant false alarm rate circuitry is to determine the 
+% power threshold above which any return can be considered to probably 
+% originate from a target as opposed to one of the spurious sources.
