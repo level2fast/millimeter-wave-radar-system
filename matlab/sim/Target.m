863598e9d8274bd153511b03925f94ff431a05ba
Steven <level2fast@gmail.com>
2024-01-14 21:10:36 -0800

    Initial commit of radar sim
    Initial commit of radar sim

--- matlab/sim/Target.m
+++ matlab/sim/Target.m
@@ -3,8 +3,8 @@ classdef Target
     % needed for a basic radar system.
     properties(Access = public)
         % motion properties
-        Plat_Pos_m   {mustBeReal, mustBeVector} = [0;0;0;]
-        Plat_Vel_m_s {mustBeReal, mustBeVector} = [0;0;0;]
+        Plat_Pos_m   {mustBeReal} = 0
+        Plat_Vel_m_s {mustBeReal} = 0
 
         % Parameter selection
         Rcs_dbsm     {mustBeReal} = 0 % target cross section(decibel square meters)
