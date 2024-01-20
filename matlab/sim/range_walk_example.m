7bcc7bf173396d82183e11c6a1ad484383c4ad1b
Steven <level2fast@gmail.com>
2024-01-19 21:21:35 -0800

    Added range walk example
    Added range walk example

---
+++ matlab/sim/range_walk_example.m
@@ -0,0 +1,9 @@
+% create radar, target, waveform, calc range and range rate
+
+% create range walk datacube
+
+% plot range walked data
+
+% remove range walk from datacube
+
+% plot data with range walk removed
